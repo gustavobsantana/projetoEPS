9503a8e88c6d15f00c1ced3bd4bc38b2b9adec9c
gustavobsantana <gustavo_sant_ana@hotmail.com>
2019-09-09 22:57:09 -0300

    new packages, try now

--- SAPJ/src/Control/Main.java
+++ SAPJ/src/Control/Main.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package sapj;
+package Control;
 
 /**
  *
  * @author Gustavo Santana
  */
-public class SAPJ {
+public class Main {
 
     /**
      * @param args the command line arguments
